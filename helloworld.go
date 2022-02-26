11e3ca87453c7c6a161cecfb47710d5b994a4f79
Jodionio Muachifi <jodionisiomuachifi@ua.pt>
2022-02-26 11:31:17 +0000

    Fisrt Program

---
+++ helloworld.go
@@ -0,0 +1,15 @@
+// This is a package
+package main
+
+//This is an import
+import (
+	"fmt"
+)
+
+func main() {
+	/*
+		The code below will print Hello World!
+		to the screen, and it is amazing
+	*/
+	fmt.Println("Hello World!")
+}
